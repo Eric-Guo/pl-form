151d3d1f4235be32039ff408cb6e2c788207bafe
Eric Guo <eric.guo@sandisk.com>
2012-05-31 14:38:46 +0800

    Add auto insert the navigation facility for form generator

--- generate_forms.rb
+++ generate_forms.rb
@@ -22,9 +22,18 @@ production_log_fields='status_code:string device:string lot_no:string reject_cod
 
 # TCM Die Attach Production log sheet DOC#: 3745-01-50001
 generate :scaffold, "Die-Attach-Log #{production_log_fields} check_type:string visual_inspection_result:string die_placement_x:float die_placement_y:float die_rotation:float die_tilt:float stack_height:float confirm_by:string #{common_fields}"
+inject_into_file 'app/views/application/_nav.html.erb', :before => "	</ul><!--FOP-->" do
+  "    <li><%= link_to 'Die Attach Production Log', die_attach_logs_path -%></li>\n"
+end
 
 # TCM Die Sorter Production log sheet DOC#: 3745-01-50002
 generate :scaffold, "Die-Sorter-Log #{production_log_fields} wafer_part_no:string wafer_id:string check_type:string visual_inspection_result:string confirm_by:string #{common_fields}"
+inject_into_file 'app/views/application/_nav.html.erb', :before => "	</ul><!--FOP-->" do
+  "    <li><%= link_to 'Die Sorter Production Log', die_sorter_logs_path -%></li>\n"
+end
 
 # TCM Printing SMT Production log sheet Ref:DOC# 3745-01-10001/3745-01-20001
 generate :scaffold, "SMT-Log #{production_log_fields} qty_in:integer qty_out:integer inspection_result:string visual_inspection_after_printing:string solder_thickness:float capacitor_shear_force:float visual_inspection_after_smt confirm_by:string #{common_fields}"
+inject_into_file 'app/views/application/_nav.html.erb', :before => "	</ul><!--FOP-->" do
+  "    <li><%= link_to 'SMT Production Log', smt_logs_path -%></li>\n"
+end
