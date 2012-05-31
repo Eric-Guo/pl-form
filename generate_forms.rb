a34a1fd51ec3ad07a235abf96e06e2e2f24fa25b
Eric Guo <eric.guo@sandisk.com>
2012-05-31 18:05:37 +0800

    add generate_form function

--- generate_forms.rb
+++ generate_forms.rb
@@ -14,26 +14,64 @@
 #User
 #Supervisor
 
-# Common Fields
-common_fields='create_badge:string create_shift_code:string update_badge:string update_shift_code:string'
+def generate_form(dept, form, fields)
+	generate :scaffold, "#{form}#{fields.collect {|k,v| " #{k}:#{v}"}.join}"
+	inject_into_file 'app/views/application/_nav.html.erb', :before => "	</ul><!--#{dept}-->" do
+	  "    <li><%= link_to '#{form.titleize}', #{form.pluralize.underscore}_path -%></li>\n"
+	end
+end
+
+common_fields = {
+	:create_badge      => :string,
+	:create_shift_code => :string,
+	:update_badge      => :string,
+	:update_shift_code => :string,
+}
 
 # Production Log Fields
-production_log_fields='status_code:string device:string lot_no:string reject_code:string remark:string'
+production_log_fields = {
+	:status_code => :string,
+	:device      => :string,
+	:lot_no      => :string,
+	:reject_code => :string,
+	:remark      => :string,
+}
 
 # TCM Die Attach Production log sheet DOC#: 3745-01-50001
-generate :scaffold, "Die-Attach-Log #{production_log_fields} check_type:string visual_inspection_result:string die_placement_x:float die_placement_y:float die_rotation:float die_tilt:float stack_height:float confirm_by:string #{common_fields}"
-inject_into_file 'app/views/application/_nav.html.erb', :before => "	</ul><!--FOP-->" do
-  "    <li><%= link_to 'Die Attach Production Log', die_attach_logs_path -%></li>\n"
-end
+die_attach_production_log_fields = {
+	:check_type               => :string,
+	:visual_inspection_result => :string,
+	:die_placement_x          => :float,
+	:die_placement_y          => :float,
+	:die_rotation             => :float,
+	:die_tilt                 => :float,
+	:stack_height             => :float,
+	:confirm_by               => :string,
+}
+generate_form 'FOP', 'DieAttachProductionLog',
+	(production_log_fields.merge die_attach_production_log_fields.merge common_fields)
 
 # TCM Die Sorter Production log sheet DOC#: 3745-01-50002
-generate :scaffold, "Die-Sorter-Log #{production_log_fields} wafer_part_no:string wafer_id:string check_type:string visual_inspection_result:string confirm_by:string #{common_fields}"
-inject_into_file 'app/views/application/_nav.html.erb', :before => "	</ul><!--FOP-->" do
-  "    <li><%= link_to 'Die Sorter Production Log', die_sorter_logs_path -%></li>\n"
-end
+die_sorter_production_log_fields = {
+	:wafer_part_no            => :string,
+	:wafer_id                 => :string,
+	:check_type               => :string,
+	:visual_inspection_result => :string,
+	:confirm_by               => :string,
+}
+generate_form 'FOP', 'DieSorterProductionLog',
+	(production_log_fields.merge die_sorter_production_log_fields.merge common_fields)
 
 # TCM Printing SMT Production log sheet Ref:DOC# 3745-01-10001/3745-01-20001
-generate :scaffold, "SMT-Log #{production_log_fields} qty_in:integer qty_out:integer inspection_result:string visual_inspection_after_printing:string solder_thickness:float capacitor_shear_force:float visual_inspection_after_smt confirm_by:string #{common_fields}"
-inject_into_file 'app/views/application/_nav.html.erb', :before => "	</ul><!--FOP-->" do
-  "    <li><%= link_to 'SMT Production Log', smt_logs_path -%></li>\n"
-end
+smt_production_log_fields = {
+	:qty_in                           => :integer,
+	:qty_out                          => :integer,
+	:inspection_result                => :string,
+	:visual_inspection_after_printing => :string,
+	:solder_thickness                 => :float,
+	:capacitor_shear_force            => :float,
+	:visual_inspection_after_smt      => :string,
+	:confirm_by                       => :string,
+}
+generate_form 'FOP', 'SMT-ProductionLog',
+	(production_log_fields.merge smt_production_log_fields.merge common_fields)
