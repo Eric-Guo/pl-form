9c55116e60a3139d97c7f203ce24135d8bfe1b2e
Eric Guo <eric.guo@sandisk.com>
2012-09-04 13:19:14 +0800

    Enable other user change record, but only once, also extend change time to 15 hours and delete time to 30 minutes.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -10,12 +10,12 @@ class Ability
       can :create, :all if user.present?
       can [:update, :destroy], :all if user.present? and user.supervisor?
       can :update, :all do |r|
-        r.create_badge == user.badge and (r.update_badge.blank? or r.update_badge == user.badge) \
-        and (Time.now - r.created_at) < 30.minutes if user.present?
+        (r.update_badge.blank? or r.update_badge == user.badge) \
+        and (Time.now - r.created_at) < 15.hours if user.present?
       end
       can [:delete,:destroy], :all do |r|
         r.create_badge == user.badge and (r.update_badge.blank? or r.update_badge == user.badge) \
-        and (Time.now - r.created_at) < 7.minutes if user.present?
+        and (Time.now - r.created_at) < 30.minutes if user.present?
       end
     end
   end
