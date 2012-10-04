6f85c52ff3f9db00b6e2d89d8f68891e89fc0c3b
Eric Guo <eric.guo@sandisk.com>
2012-10-04 15:25:53 +0800

    Show login form in home index, which make PAD/Phone more easier to use

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,4 +22,16 @@ module ApplicationHelper
   		f.input field_name.to_sym
   	end
   end
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
